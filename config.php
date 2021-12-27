f579f49868551449c7a787d643370013c65b29cd
learngiteasy <cuemath.teacher.21@gmail.com>
2021-12-27 11:20:05 +0530

    config file added

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//$keyId = 'your key id here';
+$keyId = 'use your key id here';
+$keySecret = 'use your key secret here';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
